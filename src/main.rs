041f192c38537a3bee9fadfdf6cad3f65ef92618
Oskar Goldhahn <oskar@goldhahn.info>
2024-03-10 22:00:16 +0100

    mvp projectiles

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+mod projectiles;
+
+use projectiles::{projectile_movement, ProjectileBundle};
 use bevy::{input::ButtonInput, prelude::*, sprite::MaterialMesh2dBundle, window::PrimaryWindow};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_systems(Startup, setup)
-        .add_systems(Update, keyboard_input)
-        .add_systems(Update, sync_player_and_camera_pos)
+        .add_systems(Update, (keyboard_input, sync_player_and_camera_pos, projectile_movement))
         .run();
 }
 
@@ -58,21 +60,25 @@ fn sync_player_and_camera_pos(
 }
 
 fn keyboard_input(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
     keys: Res<ButtonInput<KeyCode>>,
     mut player: Query<&mut Transform, With<Player>>,
 ) {
+    const SPEED: f32 = 5.;
+    let player_dir_x = if keys.pressed(KeyCode::KeyA) {0.} else {1.} - if keys.pressed(KeyCode::KeyD) {0.} else {1.};
+    let player_dir_y = if keys.pressed(KeyCode::KeyS) {0.} else {1.} - if keys.pressed(KeyCode::KeyW) {0.} else {1.};
     let player_position = &mut player.single_mut().translation;
-    if keys.pressed(KeyCode::KeyW) && player_position.y < 1900. {
-        player_position.y += 5.0;
-    }
-    if keys.pressed(KeyCode::KeyS) && player_position.y > -1900. {
-        player_position.y -= 5.0;
-    }
-    if keys.pressed(KeyCode::KeyD) && player_position.x < 1900. {
-        player_position.x += 5.0;
-    }
-    if keys.pressed(KeyCode::KeyA) && player_position.x > -1900. {
-        player_position.x -= 5.0;
+    const BOUND: f32 = 1900.;
+    player_position.x = (player_position.x + SPEED * player_dir_x).clamp(-BOUND, BOUND);
+    player_position.y = (player_position.y + SPEED * player_dir_y).clamp(-BOUND, BOUND);
+    if keys.pressed(KeyCode::KeyJ) {
+        commands.spawn(ProjectileBundle::new(SpriteBundle {
+            transform: Transform::from_xyz(player_position.x, player_position.y, 1.),
+            texture: asset_server.load("models/sprite1.png"),
+            ..default()
+        },
+    Vec2::new(player_dir_x, player_dir_y)));
     }
 }
 
