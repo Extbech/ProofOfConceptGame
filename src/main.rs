e814c8f98c84a1a2485638232a5757c8baa8f9f4
Extbech <betternerfakali@gmail.com>
2024-03-10 22:57:51 +0100

    Co-authored-by: oskgo <oskgo@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+mod map;
+mod player;
 mod projectiles;
-
 use bevy::{
     diagnostic::DiagnosticsStore, diagnostic::FrameTimeDiagnosticsPlugin, input::ButtonInput,
-    prelude::*, sprite::MaterialMesh2dBundle, window::PrimaryWindow,
+    prelude::*, window::PrimaryWindow,
 };
 use projectiles::{projectile_movement, ProjectileBundle};
 
@@ -10,7 +11,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugins(FrameTimeDiagnosticsPlugin::default())
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup, map::setup_map))
         .add_systems(
             Update,
             (
@@ -30,21 +31,29 @@ struct Player;
 
 /// normalized vector pointing in some direction. Is always nonzero
 #[derive(Component, Clone, Copy)]
-pub struct Direction{v: Vec2}
+pub struct Direction {
+    v: Vec2,
+}
 
 impl Direction {
     fn new(x: f32, y: f32) -> Self {
-        Direction{v: Vec2::new(x, y).try_normalize().unwrap_or(default())}
+        Direction {
+            v: Vec2::new(x, y).try_normalize().unwrap_or(default()),
+        }
     }
 
     fn try_new(x: f32, y: f32) -> Option<Self> {
-        Some(Direction{v: Vec2::new(x, y).try_normalize()?})
+        Some(Direction {
+            v: Vec2::new(x, y).try_normalize()?,
+        })
     }
 }
 
 impl Default for Direction {
     fn default() -> Self {
-        Direction { v: Vec2::new(0., 1.) }
+        Direction {
+            v: Vec2::new(0., 1.),
+        }
     }
 }
 
@@ -52,20 +61,22 @@ impl Default for Direction {
 pub struct PlayerBundle {
     marker: Player,
     dir: Direction,
-    sprite: SpriteBundle
+    sprite: SpriteBundle,
 }
 
 impl PlayerBundle {
     fn new(sprite: SpriteBundle) -> Self {
-        PlayerBundle {marker: Player, dir: default(), sprite}
+        PlayerBundle {
+            marker: Player,
+            dir: default(),
+            sprite,
+        }
     }
 }
 
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
     window: Query<&mut Window, With<PrimaryWindow>>,
 ) {
     commands.spawn((
@@ -76,21 +87,11 @@ fn setup(
         },
         MyGameCamera,
     ));
-    commands.spawn(
-        PlayerBundle::new(SpriteBundle {
-            transform: Transform::from_xyz(0., 0., 1.),
-            texture: asset_server.load("models/sprite1.png"),
-            ..default()
-        }
-    ));
-    let tile1: Handle<Image> = asset_server.load("environment/backgrounddetailed1.png");
-    let _tile2: Handle<Image> = asset_server.load("environment/backgrounddetailed2.png");
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(Mesh::from(Rectangle::default())).into(),
-        transform: Transform::default().with_scale(Vec3::new(4000., 4000., 1.)),
-        material: materials.add(ColorMaterial::from(tile1)),
+    commands.spawn(PlayerBundle::new(SpriteBundle {
+        transform: Transform::from_xyz(0., 0., 1.),
+        texture: asset_server.load("models/sprite1.png"),
         ..default()
-    });
+    }));
     app_window_config(window);
 }
 
@@ -113,20 +114,24 @@ fn keyboard_input(
 ) {
     let (mut player_trans, mut player_dir) = player.single_mut();
     const SPEED: f32 = 5.;
-    let player_dir_x = if keys.pressed(KeyCode::KeyA) {0.} else {1.} - if keys.pressed(KeyCode::KeyD) {0.} else {1.};
-    let player_dir_y = if keys.pressed(KeyCode::KeyS) {0.} else {1.} - if keys.pressed(KeyCode::KeyW) {0.} else {1.};
+    let player_dir_x = if keys.pressed(KeyCode::KeyA) { 0. } else { 1. }
+        - if keys.pressed(KeyCode::KeyD) { 0. } else { 1. };
+    let player_dir_y = if keys.pressed(KeyCode::KeyS) { 0. } else { 1. }
+        - if keys.pressed(KeyCode::KeyW) { 0. } else { 1. };
     let player_position = &mut player_trans.translation;
     const BOUND: f32 = 1900.;
     player_position.x = (player_position.x + SPEED * player_dir_x).clamp(-BOUND, BOUND);
     player_position.y = (player_position.y + SPEED * player_dir_y).clamp(-BOUND, BOUND);
     *player_dir = Direction::try_new(player_dir_x, player_dir_y).unwrap_or(*player_dir);
     if keys.pressed(KeyCode::KeyJ) {
-        commands.spawn(ProjectileBundle::new(SpriteBundle {
-            transform: Transform::from_xyz(player_position.x, player_position.y, 1.),
-            texture: asset_server.load("models/sprite1.png"),
-            ..default()
-        },
-    *player_dir));
+        commands.spawn(ProjectileBundle::new(
+            SpriteBundle {
+                transform: Transform::from_xyz(player_position.x, player_position.y, 1.),
+                texture: asset_server.load("models/sprite1.png"),
+                ..default()
+            },
+            *player_dir,
+        ));
     }
     // try to get a "smoothed" FPS value from Bevy
     if let Some(value) = diagnostics
