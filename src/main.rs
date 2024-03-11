5245b23923b9851963a2b755da72e10e27df609a
Benjamin Chandler <benjamin.chandler@bouvet.no>
2024-03-11 15:12:11 +0100

    setup ground map tiles including roads etc for proof of concept.

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,17 @@ use bevy::{
     diagnostic::DiagnosticsStore, diagnostic::FrameTimeDiagnosticsPlugin, input::ButtonInput,
     prelude::*, window::PrimaryWindow,
 };
-use player::{tick_cooldown, AttackCooldown, MaxAttackCooldown, Player, PlayerBundle, ProjectileSpeed};
+use bevy_ecs_tilemap::TilemapPlugin;
+use player::{
+    tick_cooldown, AttackCooldown, MaxAttackCooldown, Player, PlayerBundle, ProjectileSpeed,
+};
 use projectiles::{projectile_movement, ProjectileBundle};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugins(FrameTimeDiagnosticsPlugin::default())
+        .add_plugins(TilemapPlugin)
         .add_systems(Startup, (setup, map::setup_map))
         .add_systems(
             Update,
@@ -62,13 +66,13 @@ fn setup(
     commands.spawn((
         {
             let mut cam = Camera2dBundle::default();
-            cam.transform = cam.transform.with_scale(Vec3::new(3., 3., 1.));
+            cam.transform = cam.transform.with_scale(Vec3::new(1., 1., 1.));
             cam
         },
         MyGameCamera,
     ));
     commands.spawn(PlayerBundle::new(SpriteBundle {
-        transform: Transform::from_xyz(0., 0., 1.),
+        transform: Transform::from_xyz(0., 0., 1.).with_scale(Vec3::new(0.3, 0.3, 1.)),
         texture: asset_server.load("models/hero.png"),
         ..default()
     }));
@@ -90,17 +94,39 @@ fn keyboard_input(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     keys: Res<ButtonInput<KeyCode>>,
-    mut player: Query<(&mut Transform, &mut Direction, &MovementSpeed, &ProjectileSpeed, &mut AttackCooldown, &MaxAttackCooldown), With<Player>>,
+    mut player: Query<
+        (
+            &mut Transform,
+            &mut Direction,
+            &MovementSpeed,
+            &ProjectileSpeed,
+            &mut AttackCooldown,
+            &MaxAttackCooldown,
+        ),
+        With<Player>,
+    >,
     diagnostics: Res<DiagnosticsStore>,
 ) {
-    let (mut player_trans, mut player_dir, &player_speed, &projectile_speed, mut attack_cooldown, &maxcd) = player.single_mut();
+    let (
+        mut player_trans,
+        mut player_dir,
+        &player_speed,
+        &projectile_speed,
+        mut attack_cooldown,
+        &maxcd,
+    ) = player.single_mut();
     let player_position = &mut player_trans.translation;
-    let keyboard_dir_x = if keys.pressed(KeyCode::KeyA) {0.} else {1.} - if keys.pressed(KeyCode::KeyD) {0.} else {1.};
-    let keyboard_dir_y = if keys.pressed(KeyCode::KeyS) {0.} else {1.} - if keys.pressed(KeyCode::KeyW) {0.} else {1.};
-    
+    let keyboard_dir_x = if keys.pressed(KeyCode::KeyA) { 0. } else { 1. }
+        - if keys.pressed(KeyCode::KeyD) { 0. } else { 1. };
+    let keyboard_dir_y = if keys.pressed(KeyCode::KeyS) { 0. } else { 1. }
+        - if keys.pressed(KeyCode::KeyW) { 0. } else { 1. };
+
     let keyboard_dir = Vec2::new(keyboard_dir_x, keyboard_dir_y).normalize_or_zero();
     const BOUND: f32 = 1900.;
-    (player_position.x, player_position.y) = (player_position.xy() + *player_speed * time.delta_seconds() * keyboard_dir).clamp(-Vec2::splat(BOUND), Vec2::splat(BOUND)).into();
+    (player_position.x, player_position.y) = (player_position.xy()
+        + *player_speed * time.delta_seconds() * keyboard_dir)
+        .clamp(-Vec2::splat(BOUND), Vec2::splat(BOUND))
+        .into();
     *player_dir = Direction::try_new(keyboard_dir.x, keyboard_dir.y).unwrap_or(*player_dir);
     if keys.pressed(KeyCode::KeyJ) && **attack_cooldown <= 0. {
         commands.spawn(ProjectileBundle::new(
@@ -110,17 +136,17 @@ fn keyboard_input(
                 ..default()
             },
             *player_dir,
-            MovementSpeed(*projectile_speed)
+            MovementSpeed(*projectile_speed),
         ));
         **attack_cooldown += *maxcd;
     }
     // try to get a "smoothed" FPS value from Bevy
-    if let Some(value) = diagnostics
-        .get(&FrameTimeDiagnosticsPlugin::FPS)
-        .and_then(|fps| fps.smoothed())
-    {
-        println!("fps: {}", value);
-    }
+    //if let Some(value) = diagnostics
+    //    .get(&FrameTimeDiagnosticsPlugin::FPS)
+    //    .and_then(|fps| fps.smoothed())
+    //{
+    //    println!("fps: {}", value);
+    //}
 }
 
 fn app_window_config(mut window: Query<&mut Window, With<PrimaryWindow>>) {
