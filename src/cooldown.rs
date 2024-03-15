823061225292946a541bc66b43baad0f37764dcb
Oskar Goldhahn <oskar@goldhahn.info>
2024-03-15 02:00:54 +0100

    polish cooldown docs

--- src/cooldown.rs
+++ src/cooldown.rs
@@ -9,6 +9,7 @@ pub struct Cooldown {
 }
 
 impl Cooldown {
+    /// Resets the cooldown with `tot` waiting time per period and returns the number of periods elapsed
     pub fn reset(&mut self, tot: f32) -> u32 {
         let mut count = 0;
         assert!(0. < tot);
@@ -20,25 +21,28 @@ impl Cooldown {
         count
     }
 
+    /// Sets the cooldown to the waiting status, where it clamps the timer to 0 when ticked
     pub fn wait(&mut self) {
         self.waiting = true;
+    }
+
+    /// Reduce the cooldown. Clamp it if waiting and negative
+    pub fn tick(&mut self, time: &Time) {
+        if 0. < self.timer || !self.waiting {
+            self.timer -= time.delta_seconds();
+        }
+        if self.waiting && self.timer < 0. {
+            self.timer = 0.;
+        }
     }
 }
 
 pub fn tick_cooldown<CD: DerefMut<Target = Cooldown> + Component>(time: Res<Time>, mut q: Query<&mut CD>) {
     for mut cd in &mut q {
-        if 0. < cd.timer || !cd.waiting {
-            cd.timer -= time.delta_seconds();
-        } else if cd.waiting && cd.timer < 0. {
-            cd.timer = 0.;
-        }
+        cd.tick(&time);
     }
 }
 
 pub fn tick_cooldown_res<CD: DerefMut<Target = Cooldown> + Resource>(time: Res<Time>, mut cd: ResMut<CD>) {
-    if 0. < cd.timer || !cd.waiting {
-        cd.timer -= time.delta_seconds();
-    } else if cd.waiting && cd.timer < 0. {
-        cd.timer = 0.;
-    }
+    cd.tick(&time);
 }
