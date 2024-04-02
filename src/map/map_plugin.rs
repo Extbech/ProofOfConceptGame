31952d531ad58fa0deb1506f87f2dc01818149d7
Extbech <betternerfakali@gmail.com>
2024-04-02 18:37:48 +0200

    working on map

--- src/map/map_plugin.rs
+++ src/map/map_plugin.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 use noise::{NoiseFn, Perlin};
 use rand::Rng;
-use test_game::{TILE_LAYER_1_Z, TILE_LAYER_2_Z};
+use test_game::{TILE_LAYER_1_Z, TILE_LAYER_2_Z, TILE_LAYER_3_Z};
 
 const MAP_SIZE: (usize, usize) = (128, 128);
 const TILE_SIZE: Vec2 = Vec2::new(32.0, 32.0);
@@ -24,9 +24,17 @@ const PERLIN_SCALE_FACTOR: f64 = 15.0;
 ///     _ => unreachable!("Unreachable!")
 /// }
 /// ```
-enum TileSheetIndex {
+pub enum TileSheetIndex {
     MiddleGrassTile = 113,
     MiddleSandTile = 117,
+    LeftGrassTile = 1183,
+    DownGrassTile = 1182,
+    TopGrassTile = 1239,
+    RightGrassTile = 1238,
+    LeftDownGrassTile = 902,
+    LeftTopGrassTile = 846,
+    RightDownGrassTile = 903,
+    RightTopGrassTile = 847,
     Bush = 1078,
 }
 
@@ -40,6 +48,11 @@ pub struct LayerOne;
 #[derive(Component)]
 pub struct LayerTwo;
 
+#[derive(Component)]
+pub struct LayerThree;
+
+#[derive(Component)]
+pub struct TileType(pub TileSheetIndex);
 pub struct MapPlugin;
 
 impl Plugin for MapPlugin {
@@ -55,17 +68,18 @@ pub fn setup_map(
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
     seed: Res<GenerationSeed>,
 ) {
+    let map = generate_noise_map(seed.0);
     spawn_ground_layer(
         &mut commands,
         &asset_server,
         &mut texture_atlas_layouts,
-        &seed,
+        &map,
     );
     spawn_decoration_layer(
         &mut commands,
         &asset_server,
         &mut texture_atlas_layouts,
-        seed,
+        &map,
     );
 }
 
@@ -73,17 +87,15 @@ fn spawn_decoration_layer(
     commands: &mut Commands,
     asset_server: &Res<AssetServer>,
     texture_atlas_layouts: &mut ResMut<Assets<TextureAtlasLayout>>,
-    seed: Res<GenerationSeed>,
+    map: &Perlin,
 ) {
-    let map = generate_noise_map(seed.0);
-
     let texture_handle: Handle<Image> = asset_server.load("environment/map_tilesheet.png");
     let layout = TextureAtlasLayout::from_grid(TILE_SIZE, GRID_COL, GRID_ROW, None, None);
     let texture_atlas_layout = texture_atlas_layouts.add(layout);
     commands
         .spawn(())
-        .insert(Name::new("Map Layer Two"))
-        .insert(LayerTwo)
+        .insert(Name::new("Map Layer Three"))
+        .insert(LayerThree)
         .insert(Transform::default())
         .insert(GlobalTransform::default())
         .insert(InheritedVisibility::default())
@@ -107,7 +119,7 @@ fn spawn_decoration_layer(
                             transform: Transform::from_translation(Vec3::new(
                                 START_X + (x as f32 * TILE_SIZE.x),
                                 START_Y + (y as f32 * TILE_SIZE.y),
-                                TILE_LAYER_2_Z,
+                                TILE_LAYER_3_Z,
                             )),
                             ..default()
                         });
@@ -121,10 +133,8 @@ fn spawn_ground_layer(
     commands: &mut Commands,
     asset_server: &Res<AssetServer>,
     texture_atlas_layouts: &mut ResMut<Assets<TextureAtlasLayout>>,
-    seed: &Res<GenerationSeed>,
+    map: &Perlin,
 ) {
-    let map = generate_noise_map(seed.0);
-
     let texture_handle: Handle<Image> = asset_server.load("environment/map_tilesheet.png");
     let layout = TextureAtlasLayout::from_grid(TILE_SIZE, GRID_COL, GRID_ROW, None, None);
     let texture_atlas_layout = texture_atlas_layouts.add(layout);
@@ -138,14 +148,15 @@ fn spawn_ground_layer(
         .with_children(|child| {
             for x in 0..MAP_SIZE.0 {
                 for y in 0..MAP_SIZE.1 {
+                    let tile_sheet_index = get_ground_texture_index(map.get([
+                        x as f64 / PERLIN_SCALE_FACTOR,
+                        y as f64 / PERLIN_SCALE_FACTOR,
+                    ]));
                     child.spawn(SpriteSheetBundle {
                         texture: texture_handle.clone(),
                         atlas: TextureAtlas {
                             layout: texture_atlas_layout.clone(),
-                            index: get_ground_texture_index(map.get([
-                                x as f64 / PERLIN_SCALE_FACTOR,
-                                y as f64 / PERLIN_SCALE_FACTOR,
-                            ])),
+                            index: tile_sheet_index as usize,
                         },
                         sprite: Sprite {
                             custom_size: Some(TILE_SIZE),
@@ -162,10 +173,43 @@ fn spawn_ground_layer(
             }
         });
 }
-fn generate_noise_map(seed: u32) -> Perlin {
-    //let mut rng = thread_rng();
-    //let seed: u32 = rng.gen();
 
+fn spawn_rounded_edges_layer(
+    commands: &mut Commands,
+    asset_server: &Res<AssetServer>,
+    texture_atlas_layouts: &mut ResMut<Assets<TextureAtlasLayout>>,
+    map: &Perlin,
+) {
+    let texture_handle: Handle<Image> = asset_server.load("environment/map_tilesheet.png");
+    let layout = TextureAtlasLayout::from_grid(TILE_SIZE, GRID_COL, GRID_ROW, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+
+    commands
+        .spawn(())
+        .insert(Name::new("Map Layer Two"))
+        .insert(LayerTwo)
+        .insert(Transform::default())
+        .insert(GlobalTransform::default())
+        .insert(InheritedVisibility::default())
+        .with_children(|child| {
+            for x in 0..MAP_SIZE.0 {
+                for y in 0..MAP_SIZE.1 {
+                    let tile_sheet_index = get_ground_texture_index(map.get([
+                        x as f64 / PERLIN_SCALE_FACTOR,
+                        y as f64 / PERLIN_SCALE_FACTOR,
+                    ]));
+                    match tile_sheet_index {
+                        TileSheetIndex::MiddleSandTile => {
+                            // Check for bottom
+                            //if x > 0 && get_ground_texture_index(map.get([x as f64 / PERLIN_SCALE_FACTOR, ]))
+                        }
+                        _ => continue,
+                    }
+                }
+            }
+        });
+}
+fn generate_noise_map(seed: u32) -> Perlin {
     Perlin::new(seed)
 }
 
@@ -176,10 +220,10 @@ fn should_spawn_tree(val: f64) -> bool {
     }
 }
 
-fn get_ground_texture_index(val: f64) -> usize {
+fn get_ground_texture_index(val: f64) -> TileSheetIndex {
     match val.abs() {
-        v if v < 0.5 => TileSheetIndex::MiddleGrassTile as usize,
-        v if v > 0.5 => TileSheetIndex::MiddleSandTile as usize,
+        v if v < 0.5 => TileSheetIndex::MiddleGrassTile,
+        v if v > 0.5 => TileSheetIndex::MiddleSandTile,
         _ => unreachable!("no index found"),
     }
 }
