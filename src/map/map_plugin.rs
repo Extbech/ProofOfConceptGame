21cb4465a5e019066f6d22b0491f6c55099e6949
Extbech <betternerfakali@gmail.com>
2024-04-02 19:31:38 +0200

    upgrades to map edges

--- src/map/map_plugin.rs
+++ src/map/map_plugin.rs
@@ -6,12 +6,13 @@ use test_game::{TILE_LAYER_1_Z, TILE_LAYER_2_Z, TILE_LAYER_3_Z};
 const MAP_SIZE: (usize, usize) = (128, 128);
 const TILE_SIZE: Vec2 = Vec2::new(32.0, 32.0);
 const GRID_COL: usize = 56;
-const GRID_ROW: usize = 22;
+const GRID_ROW: usize = 24;
 const START_X: f32 = -(MAP_SIZE.0 as f32 * TILE_SIZE.x) / 2.0;
 const START_Y: f32 = -(MAP_SIZE.1 as f32 * TILE_SIZE.y) / 2.0;
 const PERLIN_SCALE_FACTOR: f64 = 15.0;
 
 #[repr(usize)]
+#[derive(PartialEq, Eq)]
 /// A Unit-only enum represinting the different indexes (usize) used in tilesheet.
 /// An enum is used to more easily clearify and understand what indexes are represent what in the tilesheet so that we dont have to remember each index manually.
 /// Consider changing this into e.g consts in lib.rs or something complety different.
@@ -27,15 +28,11 @@ const PERLIN_SCALE_FACTOR: f64 = 15.0;
 pub enum TileSheetIndex {
     MiddleGrassTile = 113,
     MiddleSandTile = 117,
-    LeftGrassTile = 1183,
-    DownGrassTile = 1182,
-    TopGrassTile = 1239,
-    RightGrassTile = 1238,
-    LeftDownGrassTile = 902,
-    LeftTopGrassTile = 846,
-    RightDownGrassTile = 903,
-    RightTopGrassTile = 847,
     Bush = 1078,
+    DownGrassTile = 1182,
+    LeftGrassTile = 1183,
+    RightGrassTile = 1238,
+    TopGrassTile = 1239,
 }
 
 #[derive(Resource)]
@@ -76,6 +73,12 @@ pub fn setup_map(
         &map,
     );
     spawn_decoration_layer(
+        &mut commands,
+        &asset_server,
+        &mut texture_atlas_layouts,
+        &map,
+    );
+    spawn_rounded_edges_layer(
         &mut commands,
         &asset_server,
         &mut texture_atlas_layouts,
@@ -201,7 +204,105 @@ fn spawn_rounded_edges_layer(
                     match tile_sheet_index {
                         TileSheetIndex::MiddleSandTile => {
                             // Check for bottom
-                            //if x > 0 && get_ground_texture_index(map.get([x as f64 / PERLIN_SCALE_FACTOR, ]))
+                            if y < MAP_SIZE.1
+                                && get_ground_texture_index(map.get([
+                                    x as f64 / PERLIN_SCALE_FACTOR,
+                                    (y + 1) as f64 / PERLIN_SCALE_FACTOR,
+                                ])) == TileSheetIndex::MiddleGrassTile
+                            {
+                                child.spawn(SpriteSheetBundle {
+                                    texture: texture_handle.clone(),
+                                    atlas: TextureAtlas {
+                                        layout: texture_atlas_layout.clone(),
+                                        index: TileSheetIndex::DownGrassTile as usize,
+                                    },
+                                    sprite: Sprite {
+                                        custom_size: Some(TILE_SIZE),
+                                        ..Default::default()
+                                    },
+                                    transform: Transform::from_translation(Vec3::new(
+                                        START_X + (x as f32 * TILE_SIZE.x),
+                                        START_Y + (y as f32 * TILE_SIZE.y),
+                                        TILE_LAYER_2_Z,
+                                    )),
+                                    ..default()
+                                });
+                            }
+                            // Check for Top
+                            if y > 0
+                                && get_ground_texture_index(map.get([
+                                    x as f64 / PERLIN_SCALE_FACTOR,
+                                    (y - 1) as f64 / PERLIN_SCALE_FACTOR,
+                                ])) == TileSheetIndex::MiddleGrassTile
+                            {
+                                child.spawn(SpriteSheetBundle {
+                                    texture: texture_handle.clone(),
+                                    atlas: TextureAtlas {
+                                        layout: texture_atlas_layout.clone(),
+                                        index: TileSheetIndex::TopGrassTile as usize,
+                                    },
+                                    sprite: Sprite {
+                                        custom_size: Some(TILE_SIZE),
+                                        ..Default::default()
+                                    },
+                                    transform: Transform::from_translation(Vec3::new(
+                                        START_X + (x as f32 * TILE_SIZE.x),
+                                        START_Y + (y as f32 * TILE_SIZE.y),
+                                        TILE_LAYER_2_Z,
+                                    )),
+                                    ..default()
+                                });
+                            }
+                            // Check for Left
+                            if x < MAP_SIZE.0
+                                && get_ground_texture_index(map.get([
+                                    (x + 1) as f64 / PERLIN_SCALE_FACTOR,
+                                    y as f64 / PERLIN_SCALE_FACTOR,
+                                ])) == TileSheetIndex::MiddleGrassTile
+                            {
+                                child.spawn(SpriteSheetBundle {
+                                    texture: texture_handle.clone(),
+                                    atlas: TextureAtlas {
+                                        layout: texture_atlas_layout.clone(),
+                                        index: TileSheetIndex::LeftGrassTile as usize,
+                                    },
+                                    sprite: Sprite {
+                                        custom_size: Some(TILE_SIZE),
+                                        ..Default::default()
+                                    },
+                                    transform: Transform::from_translation(Vec3::new(
+                                        START_X + (x as f32 * TILE_SIZE.x),
+                                        START_Y + (y as f32 * TILE_SIZE.y),
+                                        TILE_LAYER_2_Z,
+                                    )),
+                                    ..default()
+                                });
+                            }
+                            // Check for Right.
+                            if x > 0
+                                && get_ground_texture_index(map.get([
+                                    (x - 1) as f64 / PERLIN_SCALE_FACTOR,
+                                    y as f64 / PERLIN_SCALE_FACTOR,
+                                ])) == TileSheetIndex::MiddleGrassTile
+                            {
+                                child.spawn(SpriteSheetBundle {
+                                    texture: texture_handle.clone(),
+                                    atlas: TextureAtlas {
+                                        layout: texture_atlas_layout.clone(),
+                                        index: TileSheetIndex::RightGrassTile as usize,
+                                    },
+                                    sprite: Sprite {
+                                        custom_size: Some(TILE_SIZE),
+                                        ..Default::default()
+                                    },
+                                    transform: Transform::from_translation(Vec3::new(
+                                        START_X + (x as f32 * TILE_SIZE.x),
+                                        START_Y + (y as f32 * TILE_SIZE.y),
+                                        TILE_LAYER_2_Z,
+                                    )),
+                                    ..default()
+                                });
+                            }
                         }
                         _ => continue,
                     }
