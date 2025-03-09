34b56d5ae7ebd80e621d06c2491ec6032c94ec74
Extbech <betternerfakali@gmail.com>
2025-03-09 22:05:00 +0100

    coins

--- src/prestige/stats.rs
+++ src/prestige/stats.rs
@@ -5,6 +5,7 @@ use test_game::SAVE_FILE;
 
 #[derive(Resource, serde::Serialize, serde::Deserialize, Clone, Copy)]
 pub struct Stats {
+    pub coins: u32,
     pub damage_increase: f32,
 }
 
@@ -12,6 +13,7 @@ impl Default for Stats {
     fn default() -> Self {
         Self {
             damage_increase: 1.,
+            coins: 0,
         }
     }
 }
@@ -34,5 +36,9 @@ impl Stats {
             }
         }
         None
+    }
+
+    pub fn update_coins_amount(&mut self, amount: u32) {
+        self.coins += amount;
     }
 }
