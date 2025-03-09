570140f156e159af2ebd3294b1aebddb1d6736b2
Extbech <betternerfakali@gmail.com>
2025-03-09 11:41:58 +0100

    fps counter plugin init

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,26 @@
 /// Z-value for tile layer 1.
-pub const TILE_LAYER_1_Z: f32 = 1.0;
+pub const TILE_LAYER_1_Z: f32 = 1.;
 
 /// Z-value for tile layer 2.
-pub const TILE_LAYER_2_Z: f32 = 2.0;
+pub const TILE_LAYER_2_Z: f32 = 2.;
 
 /// Z-value for tile layer 3.
-pub const TILE_LAYER_3_Z: f32 = 3.0;
+pub const TILE_LAYER_3_Z: f32 = 3.;
 
 /// Z-value for all projectiles.
-pub const PROJECTILES_Z: f32 = 20.0;
+pub const PROJECTILES_Z: f32 = 20.;
 
 /// Z-value for player character(s).
-pub const PLAYER_Z: f32 = 10.0;
+pub const PLAYER_Z: f32 = 10.;
 
 /// Z-value for enemy characters.
-pub const ENEMY_Z: f32 = 10.0;
+pub const ENEMY_Z: f32 = 10.;
 
 /// Z-value for loot drops.
-pub const LOOT_DROPS_Z: f32 = 10.0;
+pub const LOOT_DROPS_Z: f32 = 10.;
+
+/// Z-value for FPS Counter.
+pub const FPS_COUNTER_Z: i32 = 100;
 
 /// Location of save file.
 pub const SAVE_FILE: &'static str = "save/save_file.json";
